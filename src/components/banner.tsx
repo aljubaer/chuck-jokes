92ac99bc5feefa748eb21567d8fa27604e61113a
aljubaer <bsse0812@iit.du.ac.bd>
2022-10-14 04:20:14 +0600

    fix banner width and position

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,16 +1,7 @@
-import bannerImg3 from "../assets/bitmap@3x.png"
 import "./banner.css";
 
-const style = {
-    width: "100%",
-    backgroundImage: `url(${bannerImg3})`,
-}
-
 export const Banner = () => (
-    <div style={{
-        // width: "100%",
-        // background: `url(${ bannerImg3 })`,
-    }}>
-        <img src={bannerImg3} alt="Banner" width="100%" />
+    <div className="banner">
+        {/* <img src={bannerImg3} alt="Banner" width="100%" /> */}
     </div>
 )
